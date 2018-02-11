ac890b3ca60ece89d366f64dd075902fde1b011d
josecarlos <josecarlos@w500.fritz.box>
2018-02-11 18:17:00 +0100

    put space id and access token in env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter"
+    title: 'Gatsby Default Starter',
   },
   plugins: [
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: 'gatsby-source-contentful',
       options: {
-        // spaceId: `psbin72dqapz`,
-        spaceId: `g1ihdswl9leo`,
-        // accessToken: `50d77896459e6664e69db89a5ac8e1aab7f97456aac66d89c73468963b005776`,
-        accessToken: `d80b684288865144ecbaac307729e67477b0dd6c90a782a102ba8cfa75d1afa4`
-      }
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      },
     },
-    "gatsby-plugin-react-helmet",
-    `gatsby-transformer-remark`
-  ]
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+  ],
 };
